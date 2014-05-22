a3f63f0b929337d700702f1e0b874c3e59a00c70
bangbang93 <bangbang93@163.com>
2014-05-23 03:37:44 +0800

    默认选中第一个版本

--- js/index.js
+++ js/index.js
@@ -4,18 +4,16 @@
 var fs = require('fs');
 var path = require('path');
 
-exports.index = function(){
-    global.currectDir = path.join(__dirname,'..');
-};
+exports.currectDir = path.join(__dirname,'..');
 
 exports.refreshVersionList = function(callback){
-    fs.readdir(global.currectDir + '/.minecraft/versions',function(err, files){
+    fs.readdir(exports.currectDir + '/.minecraft/versions',function(err, files){
         if (err){
             return callback(err);
         } else {
             var result = [];
             files.forEach(function(file){
-                var stat = fs.statSync(global.currectDir + '/.minecraft/versions/' + file);
+                var stat = fs.statSync(exports.currectDir + '/.minecraft/versions/' + file);
                 if (stat.isDirectory()){
                     result.push(file);
                 }
