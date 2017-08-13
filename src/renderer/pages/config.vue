c318dc5c79afcb4aed7e06cf83b2bc2ac3dc2850
bangbang93 <bangbang93@163.com>
2017-08-13 18:17:48 +0800

    设置界面……

--- src/renderer/pages/config.vue
+++ src/renderer/pages/config.vue
@@ -1,9 +1,26 @@
 <template>
   <el-row class="bmcl-setting">
     <el-col :span="4" class="bmcl-setting">
-      <el-menu mode="vertical" theme="light" class="bmcl-setting bmcl-setting-menu">
-        <el-menu-item index="0">游戏</el-menu-item>
+      <el-menu mode="vertical" theme="light" class="bmcl-setting bmcl-setting-menu" defaultActive="game">
+        <el-menu-item index="game">游戏</el-menu-item>
       </el-menu>
+    </el-col>
+    <el-col :span="20" class="bmcl-setting-container">
+      <el-card class="block" header="游戏">
+        aa
+      </el-card>
+      <el-card class="block" header="游戏">
+        aa
+      </el-card>
+      <el-card class="block" header="游戏">
+        aa
+      </el-card>
+      <el-card class="block" header="游戏">
+        aa
+      </el-card>
+      <el-card class="block" header="游戏">
+        aa
+      </el-card>
     </el-col>
   </el-row>
 </template>
@@ -22,8 +39,21 @@
 <style scoped="" lang="scss">
   .bmcl-setting {
     height: 100%;
+    .block {
+      margin: 10px 10px 0 10px;
+      border: #ccc solid 1px;
+      border-radius: 4px;
+      padding: 20px;
+      &:hover {
+        box-shadow: #ccc 2px 2px 2px 2px;
+      }
+    }
   }
   .bmcl-setting-menu {
     overflow: scroll;
+  }
+  .bmcl-setting-container {
+    overflow: scroll;
+    height: 100%;
   }
 </style>
