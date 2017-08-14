91d4998913e7d69ddb2c63661c4a95e2febc04ab
bangbang93 <bangbang93@163.com>
2017-08-14 10:21:20 +0800

    滚动动画

--- src/renderer/pages/config.vue
+++ src/renderer/pages/config.vue
@@ -1,26 +1,30 @@
 <template>
   <el-row class="bmcl-setting">
     <el-col :span="4" class="bmcl-setting">
-      <el-menu mode="vertical" theme="light" class="bmcl-setting bmcl-setting-menu" defaultActive="game">
-        <el-menu-item index="game">游戏</el-menu-item>
+      <el-menu mode="vertical" theme="light" class="bmcl-setting bmcl-setting-menu" defaultActive="game" @select="onSelect">
+        <el-menu-item index="game1">游戏1</el-menu-item>
+        <el-menu-item index="game2">游戏2</el-menu-item>
+        <el-menu-item index="game3">游戏3</el-menu-item>
+        <el-menu-item index="game4">游戏4</el-menu-item>
+        <el-menu-item index="game5">游戏5</el-menu-item>
       </el-menu>
     </el-col>
     <el-col :span="20" class="bmcl-setting-container">
       <el-button type="primary" class="btn-save">保存</el-button>
       <div style="clear: both"></div>
-      <el-card class="block" header="游戏">
+      <el-card class="block" header="游戏1" id="game1">
         aa
       </el-card>
-      <el-card class="block" header="游戏">
+      <el-card class="block" header="游戏2" id="game2">
         aa
       </el-card>
-      <el-card class="block" header="游戏">
+      <el-card class="block" header="游戏3" id="game3">
         aa
       </el-card>
-      <el-card class="block" header="游戏">
+      <el-card class="block" header="游戏4" id="game4">
         aa
       </el-card>
-      <el-card class="block" header="游戏">
+      <el-card class="block" header="游戏5" id="game5">
         aa
       </el-card>
     </el-col>
@@ -36,8 +40,23 @@
       ElButton,
       ElMenuItem,
       ElMenu,
-      ElCol}
-
+      ElCol},
+    methods: {
+      onSelect (index) {
+        const container = document.querySelector('.bmcl-setting-container');
+        const target = document.getElementById(index).offsetTop;
+        const diff = container.scrollTop - target;
+        const step = diff / 10;
+        let last;
+        function animate () {
+          container.scrollTop -= step;
+          if (container.scrollTop === last) return;
+          last = container.scrollTop;
+          requestAnimationFrame(animate);
+        }
+        animate();
+      }
+    }
   }
 </script>
 <style scoped="" lang="scss">
