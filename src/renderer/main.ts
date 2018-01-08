ed03c9b5b1e43ac2e0e9d6d81440cc392f026127
bangbang93 <bangbang93@163.com>
2018-01-08 20:40:13 +0800

    start up for ts

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import axios from 'axios';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-default/index.css';
+import 'element-ui/lib/theme-chalk/index.css';
 
-import App from './App';
+import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/index';
 import db from '../common/service/datastore';
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
-Vue.http = Vue.prototype.$http = axios;
+Vue['http'] = Vue.prototype.$http = axios;
 Vue.prototype.$db = db;
 Vue.config.productionTip = false;
 
--- test/e2e/utils.js
+++ test/e2e/utils.js
@@ -13,7 +13,7 @@ export default {
     this.timeout(10000)
     this.app = new Application({
       path: electron,
-      args: ['dist/electron/main.js'],
+      args: ['dist/electron/main.ts'],
       startTimeout: 10000,
       waitTimeout: 10000
     })
