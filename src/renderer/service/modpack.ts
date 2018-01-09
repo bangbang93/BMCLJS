1df7d8533f1b1fd75f18d5bca761089ecd43bc4c
bangbang93 <bangbang93@163.com>
2018-01-09 19:54:19 +0800

    modpack 示例

--- src/renderer/service/modpack.ts
+++ src/renderer/service/modpack.ts
@@ -1,3 +1,10 @@
+import * as fs from 'fs-extra'
+import {remote} from 'electron'
+import * as path from 'path'
+import * as rimraf from 'rimraf'
+
+const Node7z = require('node-7z')
+
 interface IModpackManifest {
   minecraft: {
     version: string,
@@ -9,8 +16,40 @@ interface IModpackManifest {
   version: string,
   author: string,
   support: string,
-  dedicate: string,
-  forceUpdate: string,
+  dedicate?: string,
+  forceUpdate: boolean,
   releaseTime: Date,
-  description: string
+  description: string,
+
+  files: [{
+    path: string,
+    sha1: string,
+  }]
 }
+
+interface IModpackUpdateManifest extends IModpackManifest {
+  rm?: string[]
+}
+
+class Modpack {
+  public manifest: IModpackManifest
+  private tempDir: string
+  constructor (public filename: string) {
+    this.tempDir = path.join(remote.app.getPath('temp'), 'modpack', Date.now().toString())
+  }
+  async read() {
+    const zip = new Node7z()
+    await zip.extractFull(this.filename, this.tempDir)
+    const manifestFile = path.join(this.tempDir, 'modpack.json')
+    if (!await fs.pathExists(manifestFile)) {
+      throw new Error('invalid modpack: modpack.json not found')
+    }
+    this.manifest = await fs.readJSON(manifestFile)
+  }
+
+  async close() {
+    return rimraf.__promisify__(this.tempDir)
+  }
+}
+
+
